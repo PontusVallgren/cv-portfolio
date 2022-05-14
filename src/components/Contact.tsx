c6f0342702963dc6f6e48dd2e53c571730930724
PontusVallgren <pontus.wallgren92@gmail.com>
2022-05-15 00:52:35 +0200

    Possible to download cv

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,28 +1,23 @@
 import React, { useState } from "react";
-import { useRouter } from "next/router";
 import { Stack, Button, Modal, Typography, Box } from "@mui/material";
 
 import DownloadIcon from "@mui/icons-material/Download";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
 
 const Contact: React.FC = () => {
   const [open, setOpen] = useState(false);
-  const router = useRouter();
 
   const toggleModal = () => {
     setOpen(!open);
-  };
-
-  const handleClick = () => {
-    // Bygg API för att ladda ner CV PDF
-    router.push("/api/hello");
   };
 
   const style = {
     position: "absolute" as "absolute",
     top: "50%",
     left: "50%",
+    width: "350px",
     transform: "translate(-50%, -50%)",
-    width: 400,
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
@@ -31,13 +26,10 @@ const Contact: React.FC = () => {
 
   return (
     <Stack direction='row' spacing={1.5} sx={{ justifyContent: "center" }}>
-      <Button
-        size='large'
-        variant='contained'
-        endIcon={<DownloadIcon />}
-        onClick={handleClick}
-      >
-        Download CV
+      <Button size='large' variant='contained' endIcon={<DownloadIcon />}>
+        <a href='/test.pdf' download>
+          Download CV
+        </a>
       </Button>
       <Button
         color='secondary'
@@ -52,8 +44,12 @@ const Contact: React.FC = () => {
           <Typography variant='h6' component='h2'>
             Contact Information
           </Typography>
-          <Typography>Email: Pontus.wallgren92@gmail.com</Typography>
-          <Typography>Phone: 073-8480569</Typography>
+          <Typography sx={{ display: "flex" }}>
+            <EmailIcon /> Pontus.wallgren92@gmail.com
+          </Typography>
+          <Typography sx={{ display: "flex" }}>
+            <PhoneIphoneIcon /> +46 73-8480569
+          </Typography>
         </Stack>
       </Modal>
     </Stack>
