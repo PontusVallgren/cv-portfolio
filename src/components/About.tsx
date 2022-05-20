2f7df44515905d91e7638df249905ff1a5cb85b6
PontusVallgren <pontus.wallgren92@gmail.com>
2022-05-20 14:33:13 +0200

    Create about text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -29,13 +29,13 @@ const About: React.FC<AboutProps> = ({ refAbout }) => {
       <Typography variant='body1' color='text.primary'>
         Hello I’m Pontus, i’m 29 years old and half through my study to become a
         system developer in JavaScript and Java at Lernia Yrkeshögskola. I’m
-        located in Skellefteå Sweden, but I’m not unavailable to move for the
-        right opportunity. On my spare time I’m an eager reader about everything
-        from biographies to history. I love to learn and that’s one thing that I
-        love about developing, there are never enough things to learn. Please
-        check out some of my code and see if i could be a resource for your
-        company. Don’t hesitate to contact me for more information, I would love
-        hearing from you!
+        located in Skellefteå Sweden, but I’m not unavailable to relocate for
+        the right opportunity. On my spare time I’m an eager reader about
+        everything from biographies to history. I love to learn and that’s one
+        thing that I love about developing, there are always new things to learn
+        and improve. Please check out some of my code and see if i could be a
+        resource for your company. Don’t hesitate to contact me for more
+        information, I would love hearing from you!
       </Typography>
     </Box>
   );
