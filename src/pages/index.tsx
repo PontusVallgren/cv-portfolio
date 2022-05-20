79c9f3fe587492220d3a29aadf6f2379ce8662d7
PontusVallgren <pontus.wallgren92@gmail.com>
2022-05-20 14:34:58 +0200

    Create page with components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,47 @@
 import type { NextPage } from "next";
+import { useRef, useState } from "react";
+import Profile from "../components/Profile";
+import { Box, useMediaQuery } from "@mui/material";
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import ToggleButtons from "../components/ToggleButtons";
+import Skills from "../components/Skills";
+import Portfolio from "../components/Portfolio";
+import Footer from "../components/Footer";
+import { useTheme } from "@mui/material/styles";
 
 const Home: NextPage = () => {
+  const theme = useTheme();
+  const [button, setButton] = useState<string>("Portfolio");
+  const desktop = useMediaQuery(theme.breakpoints.up("sm"));
+  const refProfile = useRef(null);
+  const refAbout = useRef(null);
+  const refPortfolio = useRef(null);
+
+  const handleChange = (value: string) => {
+    setButton(value);
+  };
+
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <Box>
+      <Profile refProfile={refProfile} />
+      <About refAbout={refAbout} />
+      <Contact />
+      <ToggleButtons
+        toggle={handleChange}
+        buttonInUse={button}
+        refPortfolio={refPortfolio}
+      />
+      {button == "Portfolio" ? <Portfolio /> : <Skills />}
+      <Navbar
+        toggle={handleChange}
+        refProfile={refProfile}
+        refAbout={refAbout}
+        refPortfolio={refPortfolio}
+      />
+      {desktop && <Footer />}
+    </Box>
   );
 };
 
