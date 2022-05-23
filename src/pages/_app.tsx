73808abb388123ba6acf567b1d14b8f6dbd56d45
PontusVallgren <pontus.wallgren92@gmail.com>
2022-05-24 00:59:01 +0200

    Create header with icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import Head from "next/head";
 
 const theme = createTheme({});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Pontus Vallgren</title>
+        <meta name='description' content='CV-Portfolio Pontus Vallgren' />
+        <link rel='icon' href='/icon.png' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
